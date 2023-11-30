d4b57721ba0172956532de811ddccb69e6aebc97
Felipe <felipemalentaq@hotmail.com>
2023-11-29 22:38:20 -0300

    atualização: adicionando .env

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,12 +1,12 @@
 // src/db/connection.js
-
+require('dotenv').config();
 const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
-  host: process.env.MYSQL_HOST || 'localhost',
-  port: process.env.MYSQL_PORT || 33060,
-  user: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || 'root',
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
   database: process.env.DB_NAME || 'usuarios',
   waitForConnections: true,
   connectionLimit: 10,
